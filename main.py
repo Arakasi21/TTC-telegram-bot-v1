a6a35c074b213894b035977c5501c0d612a1ce05
Arakasi21 <40834324+Arakasi21@users.noreply.github.com>
2024-04-10 13:03:27 +0500

    creating a playground for client SH

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters, CallbackContext, CallbackQueryHandler, ConversationHandler
 from commands.start import start
 from commands.register import register
-from settings.constants import TAKE_REQ, CLOSE_REQ, CLOSE_CHAT
+from settings.constants import TAKE_REQ, CLOSE_REQ, CLOSE_CHAT, SUMBIT_PHOTO, SUBMIT_TEXT, SEND_FEEDBACK
 from commands.get_requests import get_requests
 from commands.handle_photo import handle_photo
 from commands.take_request import take_request
@@ -30,8 +30,9 @@ def main() -> None:
     application = ApplicationBuilder().token(TOKEN).build()
     start_handler = CommandHandler('start', start)
     register_handler = CommandHandler('register', register)
+
     # admin functionality
-    conv_handler = ConversationHandler(
+    admin_conv_handler = ConversationHandler(
         entry_points=[CommandHandler("get_requests",get_requests)],
         states={                                                                                                                                                       
             TAKE_REQ: [CommandHandler('take_request', take_request)],
@@ -41,11 +42,16 @@ def main() -> None:
         fallbacks=[CommandHandler("cancel", cancel)],
     )
 
-    application.add_handler(conv_handler)
+    client_conv_handler = ConversationHandler(
+        # entry_points=[CommandHandler("photo_handler",photo_handler)],
+        states={
+            # Message handler, then close chat, then feedback
+        },
+        fallbacks=[CommandHandler("cancel",cancel)],
+    )
 
-    # get_requests_handler = CommandHandler('get_requests', get_requests)
-    # take_request_handler = CommandHandler('take_request', take_request)
-    # close_request_handler = CommandHandler('close_request', close_request)
+    application.add_handler(admin_conv_handler)
+    # application.add_handler(client_conv_handler)
 
     # user sending photo and creating request
     photo_handler = MessageHandler(filters.PHOTO, handle_photo)
@@ -59,10 +65,6 @@ def main() -> None:
     application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), message_handler))
     application.add_handler(CommandHandler("close", close_chat))
 
-    # admin function's handler
-    # application.add_handler(get_requests_handler)
-    # application.add_handler(take_request_handler)
-    # application.add_handler(close_request_handler)
     application.run_polling()
 
 if __name__ == "__main__":
--- settings/constants.py
+++ settings/constants.py
@@ -1,1 +1,6 @@
+# admin constants
 TAKE_REQ, CLOSE_CHAT, CLOSE_REQ  = range(3)
+
+# client constants
+SUMBIT_PHOTO, SUBMIT_TEXT, SEND_FEEDBACK = range(3)
+
